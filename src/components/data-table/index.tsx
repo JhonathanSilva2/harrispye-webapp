59e052750fba200045b835486a12250a64058071
jhonathan Silva <105667004+JhonathanSilva2@users.noreply.github.com>
2025-05-30 18:27:20 -0300

    removendo o mx-auto

--- src/components/data-table/index.tsx
+++ src/components/data-table/index.tsx
@@ -2,29 +2,29 @@
 
 import { TPayload } from "@/app/types";
 import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
 } from "@/components/ui/table";
 import {
-	ColumnDef,
-	flexRender,
-	getCoreRowModel,
-	OnChangeFn,
-	PaginationOptions,
-	PaginationState,
-	SortingState,
-	TableOptions,
-	useReactTable,
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    OnChangeFn,
+    PaginationOptions,
+    PaginationState,
+    SortingState,
+    TableOptions,
+    useReactTable,
 } from "@tanstack/react-table";
 import {
-	ChevronDown,
-	ChevronsUpDown,
-	ChevronUp,
-	CloudAlert,
+    ChevronDown,
+    ChevronsUpDown,
+    ChevronUp,
+    CloudAlert,
 } from "lucide-react";
 import { JSX, useMemo, useState } from "react";
 import { Skeleton } from "../ui/skeleton";
@@ -33,28 +33,28 @@ import Pagination from "./pagination";
 import { Searchable } from "./types";
 
 interface DataTableProps<T> {
-	searchables?: Searchable[];
-	data?: TPayload<T[]>;
-	columns: ColumnDef<T>[];
-	pagination: PaginationState;
-	paginationOptions: Pick<
-		PaginationOptions,
-		"onPaginationChange" | "rowCount"
-	>;
-	filters: Record<string, string | number>;
-	resetFilters: () => void;
-	setFilters: (
-		partialFilters: Partial<Record<string, string | number>>,
-	) => void;
-	sorting: SortingState;
-	onSortingChange: OnChangeFn<SortingState>;
-	isPending: boolean;
-	isError: boolean;
-	headerClassName?: string;
-	headerComponent?: JSX.Element;
-	titleClassName?: string;
-	title?: string;
-	meta?: TableOptions<T>["meta"];
+    searchables?: Searchable[];
+    data?: TPayload<T[]>;
+    columns: ColumnDef<T>[];
+    pagination: PaginationState;
+    paginationOptions: Pick<
+        PaginationOptions,
+        "onPaginationChange" | "rowCount"
+    >;
+    filters: Record<string, string | number>;
+    resetFilters: () => void;
+    setFilters: (
+        partialFilters: Partial<Record<string, string | number>>,
+    ) => void;
+    sorting: SortingState;
+    onSortingChange: OnChangeFn<SortingState>;
+    isPending: boolean;
+    isError: boolean;
+    headerClassName?: string;
+    headerComponent?: JSX.Element;
+    titleClassName?: string;
+    title?: string;
+    meta?: TableOptions<T>["meta"];
 }
 
 /**
@@ -90,202 +90,198 @@ interface DataTableProps<T> {
  * @see https://github.com/Balastrong/tanstack-filtered-table-demo/
  */
 export function DataTable<T>({
-	searchables,
-	data,
-	columns,
-	filters,
-	resetFilters,
-	setFilters,
-	pagination,
-	paginationOptions,
-	sorting,
-	onSortingChange,
-	isPending,
-	isError,
-	headerClassName,
-	headerComponent,
-	titleClassName,
-	title,
-	meta,
+    searchables,
+    data,
+    columns,
+    filters,
+    resetFilters,
+    setFilters,
+    pagination,
+    paginationOptions,
+    sorting,
+    onSortingChange,
+    isPending,
+    isError,
+    headerClassName,
+    headerComponent,
+    titleClassName,
+    title,
+    meta,
 }: DataTableProps<T>): JSX.Element {
-	const loader = useMemo(() => {
-		const loader = [];
-		for (let i = 1; i < pagination.pageSize; i++) {
-			loader.push(i);
-		}
-		return loader;
-	}, [pagination.pageSize]);
-	const [rowSelection, setRowSelection] = useState({});
+    const loader = useMemo(() => {
+        const loader = [];
+        for (let i = 1; i < pagination.pageSize; i++) {
+            loader.push(i);
+        }
+        return loader;
+    }, [pagination.pageSize]);
+    const [rowSelection, setRowSelection] = useState({});
 
-	const table = useReactTable({
-		columns,
-		data: data?.data ?? [],
-		getCoreRowModel: getCoreRowModel(),
-		...paginationOptions,
-		onGlobalFilterChange: (filters) => setFilters(filters),
-		manualPagination: true,
-		manualFiltering: true,
-		manualSorting: true,
-		onSortingChange,
-		rowCount: data?.rowCount,
-		onRowSelectionChange: setRowSelection,
-		meta,
+    const table = useReactTable({
+        columns,
+        data: data?.data ?? [],
+        getCoreRowModel: getCoreRowModel(),
+        ...paginationOptions,
+        onGlobalFilterChange: (filters) => setFilters(filters),
+        manualPagination: true,
+        manualFiltering: true,
+        manualSorting: true,
+        onSortingChange,
+        rowCount: data?.rowCount,
+        onRowSelectionChange: setRowSelection,
+        meta,
 
-		state: {
-			pagination,
-			sorting,
-			rowSelection,
-		},
-	});
+        state: {
+            pagination,
+            sorting,
+            rowSelection,
+        },
+    });
 
-	return (
-		<div className="mx-auto py-4">
-			<div className={headerClassName}>
-				{searchables ? (
-					<AdvancedFilter
-						searchables={searchables}
-						filters={filters}
-						resetFilters={resetFilters}
-						setFilters={setFilters}
-					/>
-				) : null}
-				{title ? (
-					<p className={`text-2xl ${titleClassName ?? ""}`}>
-						{title}
-					</p>
-				) : null}
-				{headerComponent ? headerComponent : null}
-			</div>
-			<div className="my-4 flex max-w-[80vw] overflow-auto md:w-full md:max-w-none">
-				<Table className="">
-					<TableHeader>
-						{table.getHeaderGroups().map((headerGroup) => (
-							<TableRow
-								key={headerGroup.id}
-								className="group/row"
-							>
-								{headerGroup.headers.map((header) => {
-									const metaClass =
-										header.column.columnDef?.meta
-											?.className ?? "";
-									return (
-										<TableHead
-											key={header.id}
-											className={metaClass}
-										>
-											{header.isPlaceholder ? null : (
-												<>
-													<div
-														{...{
-															className:
-																header.column.getCanSort()
-																	? "flex gap-1 justify-center items-center cursor-pointer select-none"
-																	: "",
-															onClick:
-																header.column.getCanSort()
-																	? header.column.getToggleSortingHandler()
-																	: () => {},
-														}}
-													>
-														{flexRender(
-															header.column
-																.columnDef
-																.header,
-															header.getContext(),
-														)}
-														{header.column.getCanSort()
-															? ({
-																	asc: (
-																		<ChevronUp />
-																	),
-																	desc: (
-																		<ChevronDown />
-																	),
-																	false: (
-																		<ChevronsUpDown />
-																	),
-																}[
-																	header.column.getIsSorted() as string
-																] ?? null)
-															: null}
-													</div>
-												</>
-											)}
-										</TableHead>
-									);
-								})}
-							</TableRow>
-						))}
-					</TableHeader>
-					<TableBody className="text-center">
-						{isPending
-							? loader.map((n) => (
-									<TableRow className="space-y-2" key={n}>
-										{columns.map((column, key) => {
-											return (
-												<TableCell
-													key={`${n}-${key}`}
-													className="h-10"
-												>
-													<Skeleton className="h-6 w-full" />
-												</TableCell>
-											);
-										})}
-									</TableRow>
-								))
-							: table.getRowModel().rows.map((row) => (
-									<TableRow
-										key={row.id}
-										className="group/row"
-									>
-										{row.getVisibleCells().map((cell) => {
-											const selectedBg =
-												row.getIsSelected()
-													? "bg-muted"
-													: "";
+    return (
+        <div className="py-4">
+            <div className={headerClassName}>
+                {searchables ? (
+                    <AdvancedFilter
+                        searchables={searchables}
+                        filters={filters}
+                        resetFilters={resetFilters}
+                        setFilters={setFilters}
+                    />
+                ) : null}
+                {title ? (
+                    <p className={`text-2xl ${titleClassName ?? ""}`}>
+                        {title}
+                    </p>
+                ) : null}
+                {headerComponent ? headerComponent : null}
+            </div>
+            <div className="my-4 flex max-w-[80vw] overflow-auto md:w-full md:max-w-none">
+                <Table className="">
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow
+                                key={headerGroup.id}
+                                className="group/row"
+                            >
+                                {headerGroup.headers.map((header) => {
+                                    const metaClass =
+                                        header.column.columnDef?.meta
+                                            ?.className ?? "";
+                                    return (
+                                        <TableHead
+                                            key={header.id}
+                                            className={metaClass}
+                                        >
+                                            {header.isPlaceholder ? null : (
+                                                <>
+                                                    <div
+                                                        {...{
+                                                            className:
+                                                                header.column.getCanSort()
+                                                                    ? "flex gap-1 justify-center items-center cursor-pointer select-none"
+                                                                    : "",
+                                                            onClick:
+                                                                header.column.getCanSort()
+                                                                    ? header.column.getToggleSortingHandler()
+                                                                    : () => {},
+                                                        }}
+                                                    >
+                                                        {flexRender(
+                                                            header.column
+                                                                .columnDef
+                                                                .header,
+                                                            header.getContext(),
+                                                        )}
+                                                        {header.column.getCanSort()
+                                                            ? ({
+                                                                  asc: (
+                                                                      <ChevronUp />
+                                                                  ),
+                                                                  desc: (
+                                                                      <ChevronDown />
+                                                                  ),
+                                                                  false: (
+                                                                      <ChevronsUpDown />
+                                                                  ),
+                                                              }[
+                                                                  header.column.getIsSorted() as string
+                                                              ] ?? null)
+                                                            : null}
+                                                    </div>
+                                                </>
+                                            )}
+                                        </TableHead>
+                                    );
+                                })}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody className="text-center">
+                        {isPending
+                            ? loader.map((n) => (
+                                  <TableRow className="space-y-2" key={n}>
+                                      {columns.map((column, key) => {
+                                          return (
+                                              <TableCell
+                                                  key={`${n}-${key}`}
+                                                  className="h-10"
+                                              >
+                                                  <Skeleton className="h-6 w-full" />
+                                              </TableCell>
+                                          );
+                                      })}
+                                  </TableRow>
+                              ))
+                            : table.getRowModel().rows.map((row) => (
+                                  <TableRow key={row.id} className="group/row">
+                                      {row.getVisibleCells().map((cell) => {
+                                          const selectedBg = row.getIsSelected()
+                                              ? "bg-muted"
+                                              : "";
 
-											const metaClass =
-												cell.column.columnDef?.meta
-													?.className ?? "";
-											return (
-												<TableCell
-													key={cell.id}
-													className={`h-10 ${metaClass} ${selectedBg} `}
-												>
-													{flexRender(
-														cell.column.columnDef
-															.cell,
-														cell.getContext(),
-													)}
-												</TableCell>
-											);
-										})}
-									</TableRow>
-								))}
-						{isError ? (
-							<TableRow>
-								<TableCell colSpan={9999}>
-									<div className="flex items-center justify-center gap-2">
-										<div>
-											<CloudAlert />
-										</div>
-										<div>No Data Found!</div>
-									</div>
-								</TableCell>
-							</TableRow>
-						) : null}
-					</TableBody>
-				</Table>
-			</div>
-			<Pagination
-				getCanPreviousPage={table.getCanPreviousPage}
-				getCanNextPage={table.getCanNextPage}
-				firstPage={table.firstPage}
-				previousPage={table.previousPage}
-				nextPage={table.nextPage}
-				lastPage={table.lastPage}
-				tableState={table.getState().pagination.pageIndex}
-			/>
-		</div>
-	);
+                                          const metaClass =
+                                              cell.column.columnDef?.meta
+                                                  ?.className ?? "";
+                                          return (
+                                              <TableCell
+                                                  key={cell.id}
+                                                  className={`h-10 ${metaClass} ${selectedBg} `}
+                                              >
+                                                  {flexRender(
+                                                      cell.column.columnDef
+                                                          .cell,
+                                                      cell.getContext(),
+                                                  )}
+                                              </TableCell>
+                                          );
+                                      })}
+                                  </TableRow>
+                              ))}
+                        {isError ? (
+                            <TableRow>
+                                <TableCell colSpan={9999}>
+                                    <div className="flex items-center justify-center gap-2">
+                                        <div>
+                                            <CloudAlert />
+                                        </div>
+                                        <div>No Data Found!</div>
+                                    </div>
+                                </TableCell>
+                            </TableRow>
+                        ) : null}
+                    </TableBody>
+                </Table>
+            </div>
+            <Pagination
+                getCanPreviousPage={table.getCanPreviousPage}
+                getCanNextPage={table.getCanNextPage}
+                firstPage={table.firstPage}
+                previousPage={table.previousPage}
+                nextPage={table.nextPage}
+                lastPage={table.lastPage}
+                tableState={table.getState().pagination.pageIndex}
+            />
+        </div>
+    );
 }
