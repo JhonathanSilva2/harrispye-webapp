c9c837360daa5adc57c2dc9a52a6dac58b6d025d
jhonathan Silva <105667004+JhonathanSilva2@users.noreply.github.com>
2025-05-30 19:35:11 -0300

    fix: button add hp fixed

--- src/app/i/fabrication-monitoring/_components/add-hp.tsx
+++ src/app/i/fabrication-monitoring/_components/add-hp.tsx
@@ -1,21 +1,24 @@
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { CirclePlus, Plus } from "lucide-react";
 import { useState } from "react";
 import { EditDialog } from "./edit-dialog";
 
 export function AddHp() {
-	const [isOpen, setIsOpen] = useState(false);
-	return (
-		<EditDialog
-			mode="create"
-			triggerBtn={
-				<Button onClick={() => setIsOpen(true)} className="bg-primary">
-					<Plus />
-					Add HP
-				</Button>
-			}
-			open={isOpen}
-			setOpen={setIsOpen}
-		/>
-	);
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <EditDialog
+            mode="create"
+            triggerBtn={
+                <Button
+                    onClick={() => setIsOpen(true)}
+                    size={"sm"}
+                    variant={"constructive"}
+                >
+                    <CirclePlus /> HP
+                </Button>
+            }
+            open={isOpen}
+            setOpen={setIsOpen}
+        />
+    );
 }
--- src/components/forms/date-picker-generic.tsx
+++ src/components/forms/date-picker-generic.tsx
@@ -3,64 +3,66 @@
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import {
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
 } from "@/components/ui/popover";
 import { cn } from "@/lib/utils";
+import { isValidDate } from "@/utils/is-valid-date";
 import { format, parseISO } from "date-fns";
 import { CalendarIcon } from "lucide-react";
 import { useEffect, useState } from "react";
 
 interface Props {
-	className?: string;
-	placeholder?: string;
-	value?: Date;
-	onChange?: (dateString: string) => void;
+    className?: string;
+    placeholder?: string;
+    value?: Date;
+    onChange?: (dateString: string) => void;
 }
 
 export function DatePicker({ className, placeholder, value, onChange }: Props) {
-	const [isOpen, setIsOpen] = useState(false);
-	const [date, setDate] = useState<Date | undefined>(
-		typeof value === "string" ? parseISO(value) : value,
-	);
+    const [isOpen, setIsOpen] = useState(false);
+    const [date, setDate] = useState<Date | undefined>(
+        typeof value === "string" ? parseISO(value) : value,
+    );
 
-	useEffect(() => {
-		setDate(typeof value === "string" ? parseISO(value) : value);
-	}, [value]);
+    useEffect(() => {
+        setDate(typeof value === "string" ? parseISO(value) : value);
+    }, [value]);
 
-	return (
-		<Popover open={isOpen} onOpenChange={setIsOpen}>
-			<PopoverTrigger asChild>
-				<Button
-					variant={"outline"}
-					className={cn(
-						"w-full justify-between text-left font-normal",
-						!date && "text-muted-foreground",
-						className ?? "",
-					)}
-				>
-					{date ? (
-						format(date, "dd/MM/yyyy")
-					) : (
-						<span>{placeholder ?? "Pick a date"}</span>
-					)}
-					<CalendarIcon />
-				</Button>
-			</PopoverTrigger>
-			<PopoverContent className="w-auto p-0">
-				<Calendar
-					mode="single"
-					selected={date}
-					onSelect={(selectedDate) => {
-						if (!selectedDate) return;
-						setDate(selectedDate);
-						setIsOpen(false);
-						onChange?.(format(selectedDate, "yyyy-MM-dd"));
-					}}
-					initialFocus
-				/>
-			</PopoverContent>
-		</Popover>
-	);
+    return (
+        <Popover open={isOpen} onOpenChange={setIsOpen}>
+            <PopoverTrigger asChild>
+                <Button
+                    variant={"outline"}
+                    className={cn(
+                        "w-full justify-between text-left font-normal",
+                        !date && "text-muted-foreground",
+                        className ?? "",
+                    )}
+                >
+                    {isValidDate(date) ? (
+                        format(date as Date, "dd/MM/yyyy")
+                    ) : (
+                        <span>{placeholder ?? "Pick a date"}</span>
+                    )}
+
+                    <CalendarIcon />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0">
+                <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={(selectedDate) => {
+                        if (!selectedDate) return;
+                        setDate(selectedDate);
+                        setIsOpen(false);
+                        onChange?.(format(selectedDate, "yyyy-MM-dd"));
+                    }}
+                    initialFocus
+                />
+            </PopoverContent>
+        </Popover>
+    );
 }
