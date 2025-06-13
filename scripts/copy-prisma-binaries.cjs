72b07725733ea9fd697196820bb5e18a2024ee86
Rodrigo Monteiro Roli <33501575+RodrigoMRoli@users.noreply.github.com>
2025-06-13 11:50:38 -0300

    fix: update package list in copy-prisma-binaries script to include only @prisma/client

--- scripts/copy-prisma-binaries.cjs
+++ scripts/copy-prisma-binaries.cjs
@@ -3,7 +3,7 @@ const { copyFileSync, mkdirSync, existsSync, readdirSync } = require("fs");
 const { join, basename } = require("path");
 
 // packages to inspect
-const pkgs = ["@prisma/client-hp-base", "@prisma/client-proposals"];
+const pkgs = ["@prisma/client"];
 
 console.log("🔍 CWD:", process.cwd());
 
