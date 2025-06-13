535574c6de911bfcfdf257de58a1889955955281
Rodrigo Monteiro Roli <33501575+RodrigoMRoli@users.noreply.github.com>
2025-06-13 10:24:19 -0300

    fix: update output location in Azure workflow and enable output file tracing in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,7 @@ const nextConfig: NextConfig = {
         }
         return config;
     },
+    outputFileTracing: true,
     productionBrowserSourceMaps: false,
     logging: {
         fetches: {
