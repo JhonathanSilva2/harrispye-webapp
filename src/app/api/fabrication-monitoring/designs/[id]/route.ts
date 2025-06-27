78cd154d6fe6a1b2893737a550244cc785a9afc3
Rodrigo Monteiro Roli <33501575+RodrigoMRoli@users.noreply.github.com>
2025-06-27 13:50:36 -0300

    fix: update error messages and improve file fetching logic in fabrication monitoring API

--- src/app/api/fabrication-monitoring/designs/[id]/route.ts
+++ src/app/api/fabrication-monitoring/designs/[id]/route.ts
@@ -5,102 +5,114 @@ import { readFileSync } from "fs";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-	request: NextRequest,
-	{ params }: { params: Promise<{ id: string }> },
+    request: NextRequest,
+    { params }: { params: Promise<{ id: string }> },
 ) {
-	try {
-		const { id } = await params;
-		if (!id) {
-			return NextResponse.json(
-				{ error: "Job ID must be provided" },
-				{ status: 400 },
-			);
-		}
-		if (Number.isNaN(parseInt(id))) {
-			return NextResponse.json(
-				{ error: "Invalid job ID" },
-				{ status: 400 },
-			);
-		}
-		const parsedId = parseInt(id);
+    try {
+        const { id } = await params;
+        if (!id) {
+            return NextResponse.json(
+                { error: "Design ID must be provided" },
+                { status: 400 },
+            );
+        }
+        if (Number.isNaN(parseInt(id))) {
+            return NextResponse.json(
+                { error: "Invalid Design ID" },
+                { status: 400 },
+            );
+        }
+        const parsedId = parseInt(id);
 
-		const design =
-			await prismaBase.fabrication_monitoring_designs.findUnique({
-				where: {
-					id: parsedId,
-				},
-			});
+        const design =
+            await prismaBase.fabrication_monitoring_designs.findUnique({
+                where: {
+                    id: parsedId,
+                },
+            });
 
-		if (!design) {
-			return NextResponse.json(
-				{ error: "Design not found" },
-				{ status: 404 },
-			);
-		}
+        if (!design) {
+            return NextResponse.json(
+                { error: "Design not found" },
+                { status: 404 },
+            );
+        }
 
-		const file = readFileSync(
-			`${serverEnv.STORAGE_PATH}${design.filename}`,
-		);
+        console.log(
+            `${serverEnv.NEXT_PUBLIC_URL}/api/storage/fabrication-monitoring/drawings/${design.filename}`,
+        );
+        const fetchFile = await fetch(
+            `${serverEnv.NEXT_PUBLIC_URL}/api/storage/fabrication-monitoring/drawings/${design.filename}`,
+        );
 
-		const headers = new Headers();
-		headers.set("Content-Type", "application/octet-stream");
-		headers.set(
-			"Content-Disposition",
-			`attachment; filename="${design.filename}"`,
-		);
+        const file = await fetchFile.arrayBuffer();
 
-		return new Response(file, {
-			headers,
-		});
-	} catch (err) {
-		assert(err instanceof Error);
-		return NextResponse.json({ error: err.message }, { status: 500 });
-	}
+        if (!file) {
+            return NextResponse.json(
+                { error: "File not found" },
+                { status: 404 },
+            );
+        }
+
+        const headers = new Headers();
+        headers.set("Content-Type", "application/octet-stream");
+        headers.set(
+            "Content-Disposition",
+            `attachment; filename="${design.filename}"`,
+        );
+
+        return new Response(file, {
+            headers,
+        });
+    } catch (err) {
+        assert(err instanceof Error);
+        return NextResponse.json({ error: err.message }, { status: 500 });
+    }
 }
 
 export async function DELETE(
-	request: NextRequest,
-	{ params }: { params: Promise<{ id: string }> },
+    request: NextRequest,
+    { params }: { params: Promise<{ id: string }> },
 ) {
-	try {
-		const { id } = await params;
-		if (!id) {
-			return NextResponse.json(
-				{ error: "Job ID must be provided" },
-				{ status: 400 },
-			);
-		}
-		if (Number.isNaN(parseInt(id))) {
-			return NextResponse.json(
-				{ error: "Invalid job ID" },
-				{ status: 400 },
-			);
-		}
-		const parsedId = parseInt(id);
+    try {
+        const { id } = await params;
+        if (!id) {
+            return NextResponse.json(
+                { error: "Job ID must be provided" },
+                { status: 400 },
+            );
+        }
+        if (Number.isNaN(parseInt(id))) {
+            return NextResponse.json(
+                { error: "Invalid job ID" },
+                { status: 400 },
+            );
+        }
+        const parsedId = parseInt(id);
 
-		const design =
-			await prismaBase.fabrication_monitoring_designs.findUnique({
-				where: {
-					id: parsedId,
-				},
-			});
+        const design =
+            await prismaBase.fabrication_monitoring_designs.findUnique({
+                where: {
+                    id: parsedId,
+                },
+            });
 
-		if (!design) {
-			return NextResponse.json(
-				{ error: "Design not found" },
-				{ status: 404 },
-			);
-		}
+        if (!design) {
+            return NextResponse.json(
+                { error: "Design not found" },
+                { status: 404 },
+            );
+        }
 
-		await prismaBase.fabrication_monitoring_designs.delete({
-			where: {
-				id: parsedId,
-			},
-		});
+        await prismaBase.fabrication_monitoring_designs.delete({
+            where: {
+                id: parsedId,
+            },
+        });
 
-		return NextResponse.json({ message: "Design deleted" });
-	} catch (err) {
-		assert(err instanceof Error);
-		return NextResponse.json({ error: err.message }, { status: 500 });
-	}
+        return NextResponse.json({ message: "Design deleted" });
+    } catch (err) {
+        assert(err instanceof Error);
+        return NextResponse.json({ error: err.message }, { status: 500 });
+    }
 }
