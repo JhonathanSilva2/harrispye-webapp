981573d22a9e6c3d83a1c4d5bf1fe6dec72e2273
jhonathan Silva <105667004+JhonathanSilva2@users.noreply.github.com>
2025-08-20 17:51:43 -0300

    Adjust z-index and spacing in navbar and breadcrumb
    Updated the AppNavbar and BreadCrumb components to modify z-index values and adjust spacing classes for improved layout stacking and visual consistency.

--- src/components/app-navbar/index.tsx
+++ src/components/app-navbar/index.tsx
@@ -3,16 +3,16 @@ import { ThemeToggle } from "../theme-toggle";
 import BreadCrumb from "../map-list";
 
 const AppNavbar = () => {
-	return (
-		<div className="z-45 sticky top-0 mb-2 flex items-center justify-between gap-2 bg-background p-2">
-			{/* breadcrumb => https://ui.shadcn.com/docs/components/breadcrumb */}
-			<span className="flex">
-				<SidebarTrigger />
-				<BreadCrumb />
-			</span>
-			<ThemeToggle />
-		</div>
-	);
+    return (
+        <div className="sticky top-0 z-40 mb-2 flex items-center justify-between gap-2 bg-background p-2">
+            {/* breadcrumb => https://ui.shadcn.com/docs/components/breadcrumb */}
+            <span className="flex">
+                <SidebarTrigger />
+                <BreadCrumb />
+            </span>
+            <ThemeToggle />
+        </div>
+    );
 };
 
 export default AppNavbar;
