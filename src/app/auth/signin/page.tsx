9a8d5f39c5295364e54f9c6772f3a47433fa64dc
Rodrigo Monteiro Roli <33501575+RodrigoMRoli@users.noreply.github.com>
2025-08-25 11:02:54 -0300

    refactor: update parameter types in SignIn component for better type safety

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -2,18 +2,18 @@ import { LoginForm } from "@/components/auth/login-form";
 import AuthFrame from "../_components/auth-frame";
 
 export default async function SignIn({
-	params,
-	searchParams,
+    params,
+    searchParams,
 }: {
-	params: Promise<{ slug: string }>;
-	searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+    params?: Promise<{ slug: string }>;
+    searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-	return (
-		<AuthFrame
-			title="Login"
-			description="Enter your credentials below to access your account"
-		>
-			<LoginForm />
-		</AuthFrame>
-	);
+    return (
+        <AuthFrame
+            title="Login"
+            description="Enter your credentials below to access your account"
+        >
+            <LoginForm />
+        </AuthFrame>
+    );
 }
