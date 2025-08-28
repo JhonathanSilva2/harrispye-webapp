e7f96851e10a0986df11885717d6f9fd7fa688a0
Rodrigo M Roli <rodrigo.monteiro.roli@gmail.com>
2025-08-28 17:58:35 -0300

    feat: implement keepalive API endpoint

---
+++ src/app/api/keepalive/route.ts
@@ -0,0 +1,20 @@
+// app/api/keepalive/route.ts
+import { prismaBase } from "@/db/base-client";
+import assert from "assert";
+import { NextResponse } from "next/server";
+export const runtime = "node";
+export async function GET() {
+    try {
+        await prismaBase.$queryRaw`SELECT 1`;
+        return NextResponse.json({ ok: true });
+    } catch (err) {
+        assert(err instanceof Error);
+        console.error("DB keepalive failed", err);
+        return NextResponse.json(
+            { ok: false },
+            {
+                status: 500,
+            },
+        );
+    }
+}
