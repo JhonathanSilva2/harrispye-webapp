c5b6aeb031e60ecad1404f815a71d027e00d669a
Rodrigo Monteiro Roli <33501575+RodrigoMRoli@users.noreply.github.com>
2025-09-10 12:23:37 -0300

    refactor: streamline PATCH method by removing commented code and simplifying update logic

--- src/app/api/fabrication-monitoring/[job]/[spool_id]/route.ts
+++ src/app/api/fabrication-monitoring/[job]/[spool_id]/route.ts
@@ -21,7 +21,7 @@ export async function PATCH(
         return NextResponse.json(
             { message: "User not found" },
             { status: 400 },
-        ); // Retorne uma resposta com status e mensagem
+        );
     }
     try {
         const spoolID = (await params).spool_id;
@@ -31,7 +31,6 @@ export async function PATCH(
             });
         }
 
-        // Consultando o spool
         const spool = await prismaBase.fabrication_monitoring.findUnique({
             where: { id: Number(spoolID) },
             include: {
@@ -43,7 +42,6 @@ export async function PATCH(
             return new NextResponse("Spool not found", { status: 404 });
         }
 
-        // Tratando o body
         const body = await request.json();
         if (Object.keys(body).length === 0) {
             return new NextResponse("At least one field must be provided", {
@@ -51,12 +49,10 @@ export async function PATCH(
             });
         }
 
-        // Bloqueia atualização somente do ID
         if (Object.keys(body).length === 1 && body.id) {
             return new NextResponse("ID cannot be updated", { status: 400 });
         }
 
-        // Validação do body com Zod
         const validation =
             fabricationMonitoringUpdateSpoolSchema.safeParse(body);
         if (!validation.success) {
@@ -66,7 +62,6 @@ export async function PATCH(
             );
         }
 
-        // Campos validados e prontos para atualização
         const newBody = validation.data;
 
         const clientApproval = newBody.client_approval;
@@ -90,49 +85,21 @@ export async function PATCH(
             await approvalMail(bodyForValidation);
         }
 
-        // Atualiza o spool no banco de dados
         const updateSpool = await prismaBase.fabrication_monitoring.update({
             where: { id: spool.id },
             data: {
-                spec: newBody.spec,
-                mass: newBody.mass,
-                m2_fbe: newBody.m2_fbe,
-                m2_galvanized: newBody.m2_galvanized,
-                m2_price: newBody.m2_price,
-                price_per_kg: newBody.price_per_kg,
-                fabrication_monitoring_design_id:
-                    newBody.fabrication_monitoring_design_id,
-
-                gross_spool_cost: newBody.gross_spool_cost,
-                description: newBody.description,
-                drawing_ref: newBody.drawing_ref,
-                spool_number: newBody.spool_number,
-                materials_ordered: newBody.materials_ordered,
-                materials_arrived: newBody.materials_arrived,
-                fabrication_cutting: newBody.fabrication_cutting,
-                fabrication_welding: newBody.fabrication_welding,
-                scan_3d: newBody.scan_3d,
-                ndt_complete: newBody.ndt_complete,
-                pressure_test: newBody.pressure_test,
-                internal_coating: newBody.internal_coating,
-                external_coating: newBody.external_coating,
-                packing: newBody.packing,
-                dispatch: newBody.dispatch,
-                notes: newBody.notes,
-                client_approval: newBody.client_approval,
-                manager_approval: newBody.manager_approval,
-                updated_by: updated_by,
+                ...newBody,
+                updated_by,
             },
+            select: { id: true },
         });
 
         return NextResponse.json(updateSpool, {
             headers: { "Content-Type": "application/json" },
         });
     } catch (err) {
-        if (err instanceof Error) {
-            return new NextResponse(err.message, { status: 500 });
-        }
-        return new NextResponse("Unknown error occurred", { status: 500 });
+        assert(err instanceof Error);
+        return new NextResponse(err.message, { status: 500 });
     }
 }
 
@@ -165,7 +132,6 @@ export async function DELETE(
             where: { id: spoolToDelete.id },
         });
 
-        // Retorna sucesso
         return new NextResponse(
             JSON.stringify({ message: "Spool deleted successfully" }),
             { status: 200 },
